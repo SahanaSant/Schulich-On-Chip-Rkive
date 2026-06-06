af69eda45335d43d0e55d32787a5d4592075d287
SahanaSant <sahanasanthanam7@gmail.com>
2026-06-06 13:00:26 -0600

    carry through ripple adder

--- halfAdder.sv
+++ halfAdder.sv
@@ -26,3 +26,22 @@ endmodule
 module ripple_adder 
 
 //The for loops in SystemVerilog arent sequential and through order but through logic gates
+
+module adder ( input wire[7:0] a, input wire [7:0] b, output wire [7:0] s, output wire cout);
+    genvar i; 
+    wire [8:0] carry;
+    assign cout = carry[8]; 
+
+    //carry length eight bc there are  8 different carries through each half adder and the last final result c_out as the 9th bit. 
+
+    generate //start ur for loop. this is not a sequential loop. 
+
+        assign carry [0] = 0; 
+
+        for (i=0; i<8; i=i+1) begin: adderLoop
+            //module declaration
+
+            fullAdder loopAdd (.a(a[i]), b(b[i]), .carry(carry[i]), .s(s[i]), .cout(carry[i+1]) )
+        end
+    endgenerate
+endmodule
